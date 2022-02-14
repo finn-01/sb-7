a143ea9db94f71a5d608a8e65a27e93a253cbc98
finn-01 <nguyensam576@gmail.com>
2022-02-14 17:17:00 +0700

    set up

---
+++ src/main/java/vn/finn/spring/demoapplicationconfigandanotationvalue/DemoApplicationConfigAndAnotationValueApplication.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demoapplicationconfigandanotationvalue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplicationConfigAndAnotationValueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplicationConfigAndAnotationValueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/finn/spring/demoapplicationconfigandanotationvalue/DemoApplicationConfigAndAnotationValueApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demoapplicationconfigandanotationvalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationConfigAndAnotationValueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
